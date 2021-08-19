238020342b8d5ddab0c8759f46a08440b0ee3b36
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2021-08-19 18:00:37 -0500

    envio de algunos datos

--- clases/cliente_servidor_tcp/Cliente.java
+++ clases/cliente_servidor_tcp/Cliente.java
@@ -8,6 +8,17 @@ public class Cliente{
 			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
 			DataInputStream in = new DataInputStream(conn.getInputStream());
 
+			//enviar datos
+			out.writeInt(123);
+			out.writeDouble(123123123.687974);
+			out.write("hola".getBytes());
+
+			//recibir datos
+			byte [] buffer = new byte[4];
+			in.read(buffer, 0, 4);
+			System.out.println(new String(buffer, "UTF-8"));
+			
+
 			out.close();
 			entrada.close();
 			conn.close();
--- clases/cliente_servidor_tcp/cliente.py
+++ clases/cliente_servidor_tcp/cliente.py
@@ -4,6 +4,10 @@ try:
 	conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	conn.connect(("localhost", 50000))
 
+	conn.send(123)
+	conn.send(123198731.4927237)
+	con.send("hola")
+
 	conn.close()
 except Exception as e:
 	print("Error con el socket: {}".format(e))
