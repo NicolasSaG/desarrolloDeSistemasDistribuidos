1259799807aaa67ff0565c92c9db2d959bcb4bea
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2021-11-15 14:06:04 -0600

    vista de busqueda de articulos prueba

--- tareas/carrito/cliente/src/components/Compras.js
+++ tareas/carrito/cliente/src/components/Compras.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Menu from "./Menu";
 
@@ -17,28 +17,47 @@ export default function Compras() {
       busqueda: formdata.get("busqueda"),
     };
 
+    let body = "";
+    let name;
+    let pairs = [];
+    try {
+      for (name in data) {
+        let value = data[name];
+        if (typeof value !== "string") value = JSON.stringify(value);
+        pairs.push(
+          name +
+            "=" +
+            encodeURI(value)
+              .replace(/=/g, "%3D")
+              .replace(/&/g, "%26")
+              .replace(/%20/g, "+")
+        );
+      }
+    } catch (error) {
+      alert("Error: " + error.message);
+    }
+    body = pairs.join("&");
+    console.log(body);
     axios
-      .post("/Servicio/rest/ws/articulos")
+      .post("/Servicio/rest/ws/consulta_articulos", body, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
       .then((response) => {
-        setState(response.data);
-        console.log("datos recibidos:");
-        console.log(response.data);
+        console.log(response);
+        if (response.status === 200) {
+          setState(response.data);
+          console.log(state);
+        } else {
+          alert("No se encontraron articulos que coincidan");
+        }
       })
       .catch((error) => {
-        console.log(error);
+        alert("hubo un error");
+        console.long(error);
       });
   };
-  // useEffect(() => {
-  //   axios
-  //     .post("/Servicio/rest/ws/articulos")
-  //     .then((response) => {
-  //       setState(response.data);
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // });
 
   return (
     <div>
@@ -55,16 +74,21 @@ export default function Compras() {
       </form>
       <h1>Artículos disponibles</h1>
 
-      {/* {state.map((item) => (
-        <div key={item.id}>
+      {state.map((item) => (
+        <div>
           <p>{item.descripcion}</p>
           <p>{item.precio}</p>
-          <p>{item.cantidad}</p>
-          <p>Imagen</p>
-          <img id='articulo_imagen' alt={item.id} width='200px'></img>
+          <img
+            id='articulo_imagen'
+            alt={item.descripcion}
+            width='200px'
+            src={`data:image/jpeg;base64,${item.imagen}`}
+          ></img>
           <button>Agregar al carrito</button>
+          <br />
+          <br />
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
