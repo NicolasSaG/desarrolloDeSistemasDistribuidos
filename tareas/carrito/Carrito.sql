b1fd17fa676c9b563fe72a3871ab5b65c3fa270b
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2021-11-15 18:06:03 -0600

    carrito cliente

--- tareas/carrito/Carrito.sql
+++ tareas/carrito/Carrito.sql
@@ -20,14 +20,18 @@ create table imagenes_articulo(
     id_articulo integer not null
 );
 
--- create table carrito_compra(
---     id_compra integer auto_increment primary key,
---     id_articulo integer not null,
---     cantidad integer not null
--- );
 
 alter table imagenes_articulo add foreign key(id_articulo) references articulos(id_articulo);
 create unique index articulo_1 on articulos(descripcion);
 -- alter table carrito_compra add foreign key(id_articulo) references articulos(id_articulo);
+
+--manejo de articulos en carrito
+create table carrito_compra(
+    id_compra integer auto_increment primary key,
+    id_articulo integer not null,
+    cantidad integer not null
+);
+alter table carrito_compra add foreign key(id_articulo) references articulos(id_articulo);
+
 
 
--- tareas/carrito/servidor/Servicio.java
+++ tareas/carrito/servidor/Servicio.java
@@ -138,4 +138,110 @@ public class Servicio {
             conexion.close();
         }
     }
+
+    @POST
+    @Path("alta_carrito")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response altaCarrito(@FormParam("descripcion") String descripcion, @FormParam("cantidad") int cantidad)
+            throws Exception {
+        Connection conexion = pool.getConnection();
+        int cantidad_actual = -1;
+        int id_articulo = -1;
+        try {
+            PreparedStatement stmt_1 = conexion
+                    .prepareStatement("select id_articulo, cantidad from articulos where descripcion=?");
+            try {
+                stmt_1.setString(1, descripcion);
+                ResultSet rs = stmt_1.executeQuery();
+                try {
+                    if (rs.next()) {
+                        id_articulo = rs.getInt(1);
+                        cantidad_actual = rs.getInt(2);
+                    } else {
+                        return Response.status(400)
+                                .entity(j.toJson(new Error("No hay un articulo con esa descripcion"))).build();
+                    }
+                } finally {
+                    rs.close();
+                }
+            } finally {
+                stmt_1.close();
+            }
+
+            if (cantidad <= cantidad_actual) {
+                // update de cantidad Articulos
+                PreparedStatement stmt_2 = conexion
+                        .prepareStatement("UPDATE articulos SET cantidad=? WHERE id_articulo=?");
+                try {
+                    stmt_2.setInt(1, cantidad_actual - cantidad);
+                    stmt_2.setInt(2, id_articulo);
+                    stmt_2.executeUpdate();
+                } finally {
+                    stmt_2.close();
+                }
+
+                // agregar al carrito
+                PreparedStatement stmt_3 = conexion.prepareStatement("insert into carrito_compra VALUES (0,?,?)");
+                try {
+                    stmt_3.setInt(1, id_articulo);
+                    stmt_3.setInt(2, cantidad);
+                    stmt_3.executeUpdate();
+                } finally {
+                    stmt_3.close();
+                }
+            } else {
+                return Response.status(201).entity(j.toJson(cantidad_actual)).build();
+            }
+        } catch (Exception e) {
+            return Response.status(400).entity(j.toJson(new Error(e.getMessage()))).build();
+        } finally {
+            conexion.close();
+        }
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("consulta_carrito")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultaCarrito() throws Exception {
+        Connection conexion = pool.getConnection();
+
+        try {
+
+            PreparedStatement stmt_1 = conexion.prepareStatement(
+                    "select a.descripcion, a.precio, b.imagen, c.cantidad from articulos a left outer join imagenes_articulo b on a.id_articulo=b.id_articulo left outer join carrito_compra c on a.id_articulo=c.id_articulo where c.cantidad is not null");
+            try {
+                ResultSet rs = stmt_1.executeQuery();
+                try {
+                    ArrayList<Articulo> articulos = new ArrayList<Articulo>();
+                    while (rs.next()) {
+                        Articulo art = new Articulo();
+                        art.descripcion = rs.getString(1);
+                        art.precio = rs.getFloat(2);
+                        art.imagen = rs.getBytes(3);
+                        art.cantidad = rs.getInt(4);
+                        articulos.add(art);
+                    }
+
+                    if (articulos.isEmpty()) {
+                        return Response.status(202).entity(j.toJson("No se encontraron articulos en el carrito"))
+                                .build();
+                    } else {
+                        return Response.ok().entity(j.toJson(articulos)).build();
+                    }
+                } finally {
+                    rs.close();
+                }
+            } finally {
+                stmt_1.close();
+            }
+        } catch (Exception e) {
+            return Response.status(400).entity(j.toJson(new Error(e.getMessage()))).build();
+        } finally {
+            conexion.close();
+        }
+    }
+
 }
