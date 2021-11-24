625be337e941adee6f2469f0b4b7c481bbae341a
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2021-11-24 09:14:44 -0600

    pruebas de memoria en nanillo

--- clases/memoria-distribuida/Anillo.java
+++ clases/memoria-distribuida/Anillo.java
@@ -3,17 +3,17 @@ import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Nodo {
+public class Anillo {
     static int n = 10;
     static boolean[] b;
     static long[] m;
 
-    // token-ring
+    // token
     static int nodo;
     static int num_nodos = 4;
     static Object lock = new Object();
     static boolean bloqueo = false;
-    static boolean solicitud_bloqueo = false;
+    static boolean tengo_el_token = false;
     static String[] hosts;
     static int[] puertos;
 
@@ -30,36 +30,25 @@ public class Nodo {
             try {
                 DataInputStream in = new DataInputStream(conexion.getInputStream());
                 token = in.readLong();
-
-                if (token == -2) { // actualizar
-                    long num;
-                    int pos;
-                    num = in.readLong();
-                    pos = in.readInt();
-                    synchronized (lock) {
-                        m[pos] = num;
-                    }
-                } else if (token != -1) {
-                    // System.out.println("Token recibido: " + token);
-                    token += 1;
-                    synchronized (lock) {
-                        // System.out.println("solicitud_bloqueo " + solicitud_bloqueo);
-                        if (solicitud_bloqueo) {
-                            bloqueo = true;
-                        } else {
-                            bloqueo = false;
-                        }
-                        if (!bloqueo) {
-                            bloqueo = false;
-                            // System.out.println(" --" + solicitud_bloqueo);
-                            Thread.sleep(2000);
-                            envia_mensaje(token, hosts[(nodo + 1) % num_nodos], puertos[(nodo + 1) % num_nodos]);
-                        }
-                    }
+                if (token == -1) {
+                    System.out.println("Barrera...");
+                } else if (token == -2) {
+                    // actualizacion de datos
+                    long cambio = in.readLong();
+                    int pos = in.readInt();
+                    m[pos] = cambio;
+                } else {
+                    tengo_el_token = true;
+                    System.out.println(token);
+                    if (!bloqueo)
+                        envia_token(token);
                 }
+
                 in.close();
                 conexion.close();
-            } catch (Exception e) {
+            } catch (
+
+            Exception e) {
                 System.out.println("ERROR:" + e.getMessage());
             }
         }
@@ -87,6 +76,7 @@ public class Nodo {
                 }
             }
         }
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -113,48 +103,35 @@ public class Nodo {
         }
         System.out.println("todos los nodos activados");
         if (nodo == 0) {
-            envia_mensaje(1, "localhost", 50000 + (nodo + 1) % num_nodos);
+            envia_token(1);
         }
-        long r = 0;
-        for (int i = 0; i < 1000; i++) {
-            Lock();
-
-            r = read(0);
-            System.out.println(r);
-            r++;
-            write(0, r);
-            Unlock();
-        }
-        if (nodo == 0) {
-            System.out.println(r);
-        }
-
+        // Thread.sleep(5000);
+        // Lock();
+        // System.out.println("Adquiri el bloqueo");
+        // Thread.sleep(5000);
+        // Unlock();
+        // System.out.println("libero bloqueo");
     }
 
     private static long read(int n) {
         long res = -1;
-        synchronized (lock) {
-            res = m[n];
-        }
+        res = m[n];
         return res;
     }
 
     private static void write(int n, long value) {
-        synchronized (lock) {
-            m[n] = value;
-            b[n] = true;
-        }
+        m[n] = value;
+        b[n] = true;
     }
 
     public static void Lock() throws Exception {
-        synchronized (lock) {
-            solicitud_bloqueo = true;
-        }
         for (int i = 0; i < b.length; i++) {
             b[i] = false;
         }
-        while (!bloqueo) {
-            // System.out.println("adquiriendo bloqueo...");
+
+        bloqueo = true;
+
+        while (!tengo_el_token) {
             Thread.sleep(100);
         }
     }
@@ -165,19 +142,35 @@ public class Nodo {
             if (nodo != i) {
                 for (int j = 0; j < b.length; j++) {
                     if (b[j]) {
-                        // System.out.println("actualizando array del nodo " + i + ", posicion " + j +
-                        // ", valor=" + m[j]);
                         actualiza_array(-2, m[j], j, hosts[i], puertos[i]);
                     }
                 }
             }
         }
 
-        synchronized (lock) {
-            solicitud_bloqueo = false;
-        }
         // enviar token
-        envia_mensaje(1, "localhost", 50000 + (nodo + 1) % num_nodos);
+        envia_token(1);
+        bloqueo = false;
+    }
+
+    private static void envia_token(long token) throws Exception {
+        String host = hosts[(nodo + 1) % num_nodos];
+        int puerto = puertos[(nodo + 1) % num_nodos];
+        Socket cliente = null;
+        while (true) {
+            try {
+                cliente = new Socket(host, puerto);
+                break;
+            } catch (Exception e) {
+                Thread.sleep(100);
+            }
+        }
+
+        DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+        out.writeLong(token);
+        tengo_el_token = false;
+        out.close();
+        cliente.close();
     }
 
     private static void envia_mensaje(long token, String host, int puerto) throws Exception {
@@ -212,4 +205,5 @@ public class Nodo {
         out.close();
         cliente.close();
     }
+
 }
---
+++ clases/memoria-distribuida/run2.bat
@@ -0,0 +1,6 @@
+@echo off
+
+start cmd /k java Anillo 0 localhost:50000 localhost:50001 localhost:50002 localhost:50003
+start cmd /k java Anillo 1 localhost:50000 localhost:50001 localhost:50002 localhost:50003
+start cmd /k java Anillo 2 localhost:50000 localhost:50001 localhost:50002 localhost:50003
+start cmd /k java Anillo 3 localhost:50000 localhost:50001 localhost:50002 localhost:50003
